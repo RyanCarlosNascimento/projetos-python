a09968bf62c493215eff2711fe5c905ff081352d
Ryan Carlos <ryancarlosnascimento243@gmail.com>
2025-06-26 07:52:16 -0300

    Delete Curso_Data_Science_Python/025-boxplot_idade_por_classe_titanic.py

--- Curso_Data_Science_Python/025-boxplot_idade_por_classe_titanic.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-import numpy as np
-
-data_frame = pd.read_csv('Titanic-Dataset.csv')
-sns.boxplot(x = 'Pclass', y = 'Age', data= data_frame)
-plt.show()
